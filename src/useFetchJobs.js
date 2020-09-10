efadb752a4ad284aaf7c7a10593e14b150019f59
skooma1992 <upsonjake@gmail.com>
2020-09-10 14:18:20 -0400

    adding action reducer

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,4 +1,37 @@
+import { useReducer, useEffect } from "react";
+
+const ACTIONS = {
+  MAKE_REQUEST: "make_request",
+  GET_DATA: "get_data",
+  ERROR: "error",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, job: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+
+    default:
+      return state;
+  }
+}
+
 export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+  }, [params, page]);
+
   return {
     jobs: [],
     loading: false,
