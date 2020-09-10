4b77f9d429b64b935309a1eb9d23f14e75857aae
skooma1992 <upsonjake@gmail.com>
2020-09-10 14:24:13 -0400

    adding dispatch

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,10 +1,12 @@
 import { useReducer, useEffect } from "react";
-
+import axios from "axios";
 const ACTIONS = {
   MAKE_REQUEST: "make_request",
   GET_DATA: "get_data",
   ERROR: "error",
 };
+
+const BASE_URL = "https://jobs.github.com/positions.json";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -30,6 +32,16 @@ export default function useFetchJobs(params, page) {
 
   useEffect(() => {
     dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
   }, [params, page]);
 
   return {
