0c824ff881cac50e5d8e7b16e2b646945b574e02
skooma1992 <upsonjake@gmail.com>
2020-09-11 13:32:18 -0400

    bootstrap add

--- src/Job.js
+++ src/Job.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { Card } from "react-bootstrap";
 export default function Job({ job }) {
   return (
-    <div>
-      {job.title}--{job.type}--{job.location}
-    </div>
+    <Card>
+      <Card.Body>
+        <div className="d-flex justfiy-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
